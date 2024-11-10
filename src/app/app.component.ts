a2fc5056e9be19e8fd2010c69cab230c210298a9
cristobal.jjg.00 <cristobal.jjg.00@gmail.com>
2024-11-10 09:31:20 +0000

    Bases para la aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { YoutubeService } from './services/youtube.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'NanoTube';
+
+  constructor(ys: YoutubeService){
+    //ys.searchByKeyword('react')
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GalleryPage } from './pages/gallery/gallery.page';
+import { VideoPage } from './pages/video/video.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',   redirectTo: '/galeria', pathMatch: 'full' },
+    { path: 'galeria', component: GalleryPage },
+    { path: 'video/:id', component: VideoPage },
+    //{ path: '**', component: PageNotFoundComponent }
+];
