6ad0206f4d7eae91ebc3d449f1e027a19375938f
Cristobal <cristobal_jjg@hotmail.com>
2025-07-19 23:26:27 +0100

    Reemplazar componente Sidebar por Header en la estructura de la aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeaderComponent } from './components/header/header.component';
 import { YoutubeService } from './services/youtube.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'NanoTube';
 
-  constructor(ys: YoutubeService){
+  constructor(ys: YoutubeService) {
     //ys.searchByKeyword('react')
   }
 }
